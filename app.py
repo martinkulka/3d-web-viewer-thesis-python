7467c7217013b793c88532d60a64660b1322b7a3
martinkulka <martin.kulka481@gmail.com>
2023-03-19 17:55:28 +0100

    fixed cors

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ from fast_marching import fast_marching_segmentation
 
 app = FastAPI()
 
-origins = ["http://localhost/", "http://localhost:5173/", "http://127.0.0.1:5173"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
